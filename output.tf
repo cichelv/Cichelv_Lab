30d4314c5344c7ecc9f1f7aa8c21e1dbf56dfa3c
cichelv <valeriacichello@outlook.it>
2025-10-06 10:27:45 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,14 +1,7 @@
-output "virtual_machine_new_output" {
+output "virtual_machine_output" {
   value = {
     for k, v in module.virtual_machine : k => {
       vm = v.vm
     }
   }
-}
-
-output "virtual_machine_output" {
-  value = merge(
-    { for k, v in module.virtual_machine : k => { vm = v.vm } },
-    { for k, v in module.virtual_machine : k => { vm = v.vm } }
-  )
 }
