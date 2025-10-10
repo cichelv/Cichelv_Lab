49b1d64d62f3639290c119b01fe3ca57cb7a7abb
cichelv <valeriacichello@outlook.it>
2025-10-10 13:39:38 +0000

    22

--- modules/vm-module/main.tf
+++ modules/vm-module/main.tf
@@ -1,26 +1,26 @@
 resource "google_compute_instance" "default" {
-  "name" = var.vm_name
-  "machine_type"= var.machine_type
+  name         = var.vm_name
+  machine_type = var.machine_type
 
   boot_disk {
      initialize_params {
-    "image"= var.vm_image
-    "size" = var.boot_size
-    "type" = var.boot_type
+    image  = var.vm_image
+    size   = var.boot_size
+    type   = var.boot_type
   } },
   labels {
-    "zone" = var.zone
-    "os"   = var.vm_image
+    zone   = var.zone
+    os     = var.vm_image
     
   },
  
     network_interface {
-    "network"       = var.network
-    "access_config" = var.access_config
+   network       = var.network
+   access_config = var.access_config
   },
-  "service_account": {
-    "email"  = var.email_service_account
-    "scopes" = [var.scopes]
+  service_account: {
+    email  = var.email_service_account
+    scopes = [var.scopes]
   }
 }
 
