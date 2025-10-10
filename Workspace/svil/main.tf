116a75d25b0c49a668b40a0d135efdd130a0edd1
cichelv <valeriacichello@outlook.it>
2025-10-10 17:26:36 +0200

    Update main.tf

--- Workspace/svil/main.tf
+++ Workspace/svil/main.tf
@@ -2,8 +2,21 @@
 module "vm-module" {
   source = "git::https://github.com/cichelv/Cichelv_Lab.git//modules/vm-module?ref=Template"
 
-zone =var.zone
-machine_type = var.machine_type
-project_id = var.project_id
+project_id             = "progettocloud-473207"
+  zone                   = "us-central1-c"
+  region                 = "us-central1"
+  vm_name                = "istanza-demo"
+  machine_type           = "e2-medium"
+  vm_image               = "debian-12-bookworm-v20250910"
+  boot_size              = 10
+  boot_type              = "pd-standard"
+  network                = "default"
+  email_service_account  = "1075799113945-compute@developer.gserviceaccount.com"
+  scopes                 = "https://www.googleapis.com/auth/cloud-platform"
+  gcp_services_list      = ["compute.googleapis.com", "iam.googleapis.com"]
+
+
+
+
 
 }
