14bb115982acd9fbc099e36f1bcf6d00e54e540e
cichelv <valeriacichello@outlook.it>
2025-10-10 16:34:54 +0200

    Update backend.tf

--- Workspace/svil/backend.tf
+++ Workspace/svil/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-  backend "gcs" {
-    bucket  = "tf-state-svil"
-    prefix  = "terraform/state"
-  }
+  #backend "gcs" {
+  #  bucket  = "tf-state-svil"
+   # prefix  = "terraform/state"
+  #}
   required_providers {
     google ={
     version = "~>5.0"    
